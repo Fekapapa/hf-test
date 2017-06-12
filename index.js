783050d4e31e655e86b8040e795b7f6ee003c6e0
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-06-12 21:06:27 +0200

    index.js port modified: added OR 9876

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-var port = process.env.PORT || 8080
+var port = process.env.PORT || 8080 || 9876
 
 app.use(express.static(__dirname + '/dist'));
 
